a81d50e25b13be77d1602d5d66323763ee2018cc
ange-la <angela.lam938@gmail.com>
2021-12-02 02:17:24 -0500

    Figuring out the different options

--- plant-move.js
+++ plant-move.js
@@ -22,14 +22,16 @@ $(document).ready(function(){
 $(document).ready(function(){
   $(".plant-button").click(function(){
     $(".plant-button").hide();
-    $(".water-button").show();
-    $(".sunlight-button").show();
+    $(".water-button-1").show();
+    $(".sunlight-button-1").show();
     $(".leave-button-1").show();
     $(".small-soil-nurture").show();
   });
 });
 
 
+
+/* ORGANIZE THIS DIFFERENTLY by firstClicked1, firstClicked2, etc */
 
 
 /* STAGE 1 appears after WATER + SUNLIGHT + LEAVE all clicked in order */
@@ -41,29 +43,39 @@ $(function(){
   var thirdClicked;
 /* WATER button darkens the soil */
 /* @ https://stackoverflow.com/questions/2001366/how-can-i-change-the-text-color-with-jquery */
-$(".water-button").click(function() {
+$(".water-button-1").click(function() {
   firstClicked = true;
   $(".soil-ground").css({'background-color': '#201613'});
   $(".small-soil-nurture").css({'background-color': '#160c09'});
-  if(fifthClicked) {
+  if(fourthClicked) {
 /* STAGE 4 */
+    fifthClicked = true;
     /* remove(); to prevent stage 3 from showing again after */
-    sixthClicked = true;
     $(".stage-3").remove();
     $(".stage-4").show();
   }
+/* STAGE 8 */
+  if(ninthClicked) {
+    tenthClicked = true;
+    /* remove(); to prevent stage 7 from showing again after */
+    $(".leave-button-6").remove();
+    $(".leave-button-7").show();
+  }
 });
 /* STAGE 1 */
-$(".sunlight-button").click(function() {
+$(".sunlight-button-1").click(function() {
   if(firstClicked) {
     secondClicked = true;
     $(".stage-1").show();
     $(".soil-ground").css({'background-color': '#2d231f'});
     $(".small-soil-nurture").css({'background-color': '#1e1715'});
   }
-  if (thirdClicked){
+  if (thirdClicked) {
     $(".soil-ground").css({'background-color': '#5a453f'});
     $(".small-soil-nurture").css({'background-color': '#4b3a34'});
+    /* SWITCH SUNLIGHT" BUTTON WITH NEW */
+    $(".sunlight-button-1").remove();
+    $(".sunlight-button-2").show();
   }
 });
 /* STAGE 2 */
@@ -81,51 +93,115 @@ $(".leave-button-1").click(function() {
     $(".leave-button-2").show();
   }
 });
-/* IN BETWEEN STAGE 2 AND 3 */
+/* STAGE 3 */
 $(".leave-button-2").click(function() {
   if (thirdClicked) {
     fourthClicked = true;
     $(".soil-ground").css({'background-color': '#4b3934'});
     $(".small-soil-nurture").css({'background-color': '#2d231f'});
+    /* remove(); to prevent stage 2 from showing again after */
+    $(".stage-2").remove();
+    $(".stage-3").show();
     /* SWITCH "LEAVE" BUTTON WITH NEW */
     $(".leave-button-2").remove();
     $(".leave-button-3").show();
   }
 });
-/* STAGE 3 */
+/* IN BETWEEN STAGE 4 AND 5 */
 $(".leave-button-3").click(function() {
-  if (fourthClicked) {
-    fifthClicked = true;
-    /* remove(); to prevent stage 2 from showing again after */
-    $(".stage-2").remove();
-    $(".stage-3").show();
-    $(".soil-ground").css({'background-color': '#5a453f'});
-    $(".small-soil-nurture").css({'background-color': '#3c2f2a'});
+  if (fifthClicked) {
+    sixthClicked = true;
+    $(".soil-ground").css({'background-color': '#30211d'});
+    $(".small-soil-nurture").css({'background-color': '#24140f'});
     /* SWITCH "LEAVE" BUTTON WITH NEW */
     $(".leave-button-3").remove();
     $(".leave-button-4").show();
   }
 });
-/* IN BETWEEN STAGE 4 AND 5 */
+/* STAGE 5 */
 $(".leave-button-4").click(function() {
   if (sixthClicked) {
     seventhClicked = true;
-    $(".soil-ground").css({'background-color': '#30211d'});
-    $(".small-soil-nurture").css({'background-color': '#160c09'});
+    $(".soil-ground").css({'background-color': '#402c26'});
+    $(".small-soil-nurture").css({'background-color': '#361e17'});
     /* SWITCH "LEAVE" BUTTON WITH NEW */
     $(".leave-button-4").remove();
     $(".leave-button-5").show();
-  }
-});
-/* STAGE 5 */
-$(".leave-button-5").click(function() {
-  if (seventhClicked) {
-    eighthClicked = true;
-    $(".soil-ground").css({'background-color': '#402c26'});
-    $(".small-soil-nurture").css({'background-color': '#24140f'});
-    /* remove(); to prevent stage 2 from showing again after */
+    /* remove(); to prevent stage 4 from showing again after */
     $(".stage-4").remove();
     $(".stage-5").show();
+  }
+});
+/* IN BETWEEN STAGES 5 AND 6 */
+$(".sunlight-button-2").click(function() {
+  if (seventhClicked) {
+    eighthClicked = true;
+    $(".soil-ground").css({'background-color': '#30211d'});
+    $(".small-soil-nurture").css({'background-color': '#24140f'});
+  }
+});
+/* STAGE 6 */
+$(".leave-button-5").click(function() {
+  if (eighthClicked) {
+    ninthClicked = true;
+    $(".soil-ground").css({'background-color': '#402c26'});
+    $(".small-soil-nurture").css({'background-color': '#361e17'});
+    /* SWITCH "LEAVE" BUTTON WITH NEW */
+    $(".leave-button-5").remove();
+    $(".leave-button-6").show();
+    /* remove(); to prevent stage 5 from showing again after */
+    $(".stage-5").remove();
+    $(".stage-6").show();
+  }
+});
+/* STAGE 7 */
+$(".leave-button-6").click(function() {
+  if (eighthClicked) {
+    ninthClicked = true;
+    $(".soil-ground").css({'background-color': '#604239'});
+    $(".small-soil-nurture").css({'background-color': '#593226'});
+    /* SWITCH "LEAVE" BUTTON WITH NEW */
+    $(".leave-button-6").remove();
+    $(".leave-button-7").show();
+    /* remove(); to prevent stage 6 from showing again after */
+    $(".stage-6").remove();
+    $(".stage-7").show();
+  }
+});
+/* STAGE 9 */
+$(".leave-button-7").click(function() {
+  if(tenthClicked) {
+    eleventhClicked = true;
+    $(".soil-ground").css({'background-color': '#402c26'});
+    $(".small-soil-nurture").css({'background-color': '#361e16'});
+    /* SWITCH "LEAVE" BUTTON WITH NEW */
+    $(".leave-button-7").remove();
+    $(".leave-button-8").show();
+    /* remove(); to prevent stage 8 from showing again after */
+    $(".stage-8").remove();
+    $(".stage-9").show();
+  }
+});
+/* STAGE 10 */
+$(".leave-button-8").click(function() {
+  if(eleventhClicked) {
+    twelevthClicked = true;
+    $(".soil-ground").css({'background-color': '#604239'});
+    $(".small-soil-nurture").css({'background-color': '#5b3225'});
+    /* remove(); to prevent stage 9 from showing again after */
+    $(".stage-9").remove();
+    $(".stage-10").show();
+  }
+});
+/* STAGE 11 */
+$(".water-button-2").click(function() {
+  if(twelevthClicked) {
+    thirteenthClicked = true;
+    $(".soil-ground").css({'background-color': '#201613'});
+    $(".small-soil-nurture").css({'background-color': '#160c09'});
+    /* remove(); to prevent stage 9 from showing again after */
+    $(".stage-10").remove();
+    $(".stage-11").show();
   }
 });
 });
