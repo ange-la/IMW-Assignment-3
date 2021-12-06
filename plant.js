f545ac219814d7fc6a5dd17ca4e0e9015b01f7e1
ange-la <angela.lam938@gmail.com>
2021-12-05 19:18:48 -0500

    Changing colour of soil depending on click

--- plant.js
+++ plant.js
@@ -32,13 +32,18 @@ $(document).ready(function(){
 
 /* Simpler solution (Arrays) by Professor Ali Qadeer */
 /* OUTCOME 1 */
-const nurtureSequence1 = ["plant","water","sunlight","leave","water","water","water","water","water","water","water","water","water","water","water",]
+const nurtureSequence1 = ["plant","water","sunlight","leave","leave","water"]
 
 let clickNumber = 0;
 
-var firstWater;
-var firstSunlight;
-var firstLeave;
+var water;
+var sunlightOne;
+var sunlightTwo;
+var leaveOne;
+var leaveTwo;
+var leaveThree;
+var leaveFour;
+var leaveFive;
 
 $(function(){
   $("button").click(function(){
@@ -46,51 +51,105 @@ $(function(){
     if (buttonClicked == nurtureSequence1[clickNumber]){
       console.log("true!")
       $(".stage-1").attr("src","IMW-A3-Elements/corpse-flower-"+clickNumber+".svg")
-/* Things that appear if "true" */
-      /* COLOUR OF THE SOIL */
-      /* if WATERED FIRST */
-      /* ".water-button" darkens the soil */
+/* Things appear below here if "true" */
+
+/* Trigger event after 2 buttons clicked in order */
+/* @ https://stackoverflow.com/questions/38418062/how-can-i-trigger-an-event-after-two-different-buttons-have-been-clicked */
+/* WATER button darkens the soil */
+/* @ https://stackoverflow.com/questions/2001366/how-can-i-change-the-text-color-with-jquery */
+
+/* COLOUR OF THE SOIL */
+/* ".water-button" darkens the soil */
       $(".water-button").click(function(){
-        firstWater = true;
+        water = true;
         $(".soil-ground").css({'background-color': '#201613'});
-        $(".small-soil-nurture").css({'background-color': '#160c09'});
+        $(".small-soil-nurture").css({'background-color': '#100b0a'});
       });
-      /* ".sunlight-button" AFTER ".water-button" lightens soil by 2 */
+
+/* ".sunlight-button" lightens the soil by 2 */
+      /* Watered before Sunlight */
       $(".sunlight-button").click(function(){
-        firstSunlight = true;
-        if (firstWater){
+        sunlightOne = true;
+        if (water){
           $(".soil-ground").css({'background-color': '#402c26'});
-          $(".small-soil-nurture").css({'background-color': '#361e16'});
+          $(".small-soil-nurture").css({'background-color': '#2f201d'});
         }
+        /* Sunlight right after Sunlight once */
+        $(".sunlight-button").click(function(){
+          sunlightTwo = true;
+          if (sunlightOne){
+            $(".soil-ground").css({'background-color': '#604239'});
+            $(".small-soil-nurture").css({'background-color': '#4f3630'});
+          }
+        /* Sunlight right after Sunlight twice */
+        /* MAX SOIL LIGHTNESS */
+        /* EXTREMELY UNDERWATERED */
+          $(".sunlight-button").click(function(){
+            if (sunlightTwo) {
+              $(".soil-ground").css({'background-color': '#80584d'});
+              $(".small-soil-nurture").css({'background-color': '#6f4c44'});
+            }
+          });
+        });
       });
-      /* ".leave-button" AFTER ".water-button" lightens soil by 1 */
+
+
+/* ".leave-button" lightens the soil by 1 */
+      /* Leave right after Water */
       $(".leave-button").click(function(){
-        firstLeave = true;
-        if (firstWater){
+        leaveOne = true;
+        if (water){
           $(".soil-ground").css({'background-color': '#30211d'});
-          $(".small-soil-nurture").css({'background-color': '#24140f'});
+          $(".small-soil-nurture").css({'background-color': '#1f1614'});
         }
+        /* Leave right after Leave once */
+        $(".leave-button").click(function(){
+          leaveTwo = true;
+          if (leaveOne){
+            $(".soil-ground").css({'background-color': '#402c26'});
+            $(".small-soil-nurture").css({'background-color': '#2f211e'});
+          }
+        /* Leave right after Leave twice */
+          $(".leave-button").click(function(){
+            leaveThree = true;
+            if (leaveTwo){
+              $(".soil-ground").css({'background-color': '#503730'});
+              $(".small-soil-nurture").css({'background-color': '#3e2c28'});
+            }
+        /* Leave right after Leave three times */
+            $(".leave-button").click(function(){
+              leaveFour = true;
+              if (leaveThree){
+                $(".soil-ground").css({'background-color': '#604239'});
+                $(".small-soil-nurture").css({'background-color': '#4e3732'});
+              }
+        /* Leave right after Leave four times */
+              $(".leave-button").click(function(){
+                leaveFive = true;
+                if (leaveFour){
+                  $(".soil-ground").css({'background-color': '#704d43'});
+                  $(".small-soil-nurture").css({'background-color': '#5d423c'});
+                }
+        /*Leave right after Leave five times */
+        /* MAX SOIL LIGHTNESS */
+        /* EXTREMELY UNDERWATERED */
+                $(".leave-button").click(function(){
+                  if (leaveFive){
+                    $(".soil-ground").css({'background-color': '#80584d'});
+                    $(".small-soil-nurture").css({'background-color': '#6d4d46'});
+                  }
+                });
+              });
+            });
+          });
+        });
       });
-      /* if SUNLIGHT BEFORE LEAVE */
-      $(".leave-button").click(function(){
-        secondSunlight = true;
-        if (firstSunlight){
-          $(".soil-ground").css({'background-color': '#503730'});
-          $(".small-soil-nurture").css({'background-color': '#48281e'});
-        }
-      });
-      /* if LEAVE BEFORE SUNLIGHT */
-      $(".sunlight-button").click(function(){
-        if (firstLeave){
-          $(".soil-ground").css({'background-color': '#604239'});
-          $(".small-soil-nurture").css({'background-color': '#5a3226'});
-        }
-      });
+
 
 
     } else {
       console.log("false!")
-/* Things that appear if "false" */
+/* Things appear below here if "false" */
 
 
 
